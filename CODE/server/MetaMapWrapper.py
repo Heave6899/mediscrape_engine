12e10783370537e88249fdc3b03406076ef4c554
KuoChiaCheng0318 <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-04-09 12:28:54 -0700

    add doxygen comments

--- CODE/server/MetaMapWrapper.py
+++ CODE/server/MetaMapWrapper.py
@@ -1,16 +1,32 @@
 from pymetamap import MetaMap
 import skr_web_api as sk
-class MetaMapWrapper(object):
-    mm = MetaMap.get_instance('/home/parallels/Desktop/mediscrape/public_mm/bin/metamap20')
-    def __init__(self):
-        self.key = '40d89954-8969-4fe1-8687-a463ab0a261f'
-        self.email = 'vpatel44@asu.edu'
-        self.subm = sk.Submission(self.email,self.key)
-        pass
 
-    def online_annotate(self,text):
-        self.subm.init_mm_interactive(text)
-        response = self.subm.submit()
+class MetaMapWrapper(object):
+    """
+    A class for wrapping the MetaMap API for extracting medical concepts from text.
+    """
+    mm = MetaMap.get_instance('/home/parallels/Desktop/mediscrape/public_mm/bin/metamap20')
+
+    def __init__(self):
+        """
+        Initializes a new instance of the MetaMapWrapper class.
+        """
+        self.key = '40d89954-8969-4fe1-8687-a463ab0a261f'  # API key
+        self.email = 'vpatel44@asu.edu'  # email associated with the API key
+        self.subm = sk.Submission(self.email, self.key)  # API submission object
+
+    def online_annotate(self, text):
+        """
+        Annotates text using the MetaMap API in online mode.
+
+        Args:
+            text (str): The text to be annotated.
+
+        Returns:
+            A dictionary containing the extracted medical concepts.
+        """
+        self.subm.init_mm_interactive(text)  # initialize the MetaMap API submission
+        response = self.subm.submit()  # submit the request to the API
         extracted_data = {}
         symptoms = []
         diseases = []
@@ -19,14 +35,13 @@ class MetaMapWrapper(object):
         for concept in decoded_body:
             if len(concept) > 6:
                 if concept[5] == '[sosy]':
-                        # Sign or Symptom
-                        # sometimes it returns symptoms as a symptom
+                    # Sign or Symptom
+                    # sometimes it returns symptoms as a symptom
                     if concept[3] != 'Symptoms' and concept[3] != 'symptoms':
                         symptoms.append(concept[3])
                 elif concept[5] == '[dsyn]':
                     # Disease or Syndrome
                     diseases.append(concept[3])
-                    pass
                 elif concept[5] == '[diap]':
                     # Diagnostic Procedure
                     diagnostics.append(concept[3])
@@ -41,6 +56,15 @@ class MetaMapWrapper(object):
 
 
     def annotate(self, text):
+        """
+        Annotates text using the MetaMap API in local mode.
+
+        Args:
+            text (str): The text to be annotated.
+
+        Returns:
+            A dictionary containing the extracted medical concepts.
+        """
         mm_request = [text]
         concepts, error = self.mm.extract_concepts(mm_request, [1, 2])
         extracted_data = {}
@@ -49,7 +73,6 @@ class MetaMapWrapper(object):
         diagnostics = []
         for concept in concepts:
             if hasattr(concept, 'semtypes'):
-                # print(concept)
                 if concept.semtypes == '[sosy]':
                     # Sign or Symptom
                     # sometimes it returns symptoms as a symptom
@@ -58,7 +81,6 @@ class MetaMapWrapper(object):
                 elif concept.semtypes == '[dsyn]':
                     # Disease or Syndrome
                     diseases.append(concept.preferred_name)
-                    pass
                 elif concept.semtypes == '[diap]':
                     # Diagnostic Procedure
                     diagnostics.append(concept.preferred_name)
--- CODE/server/sd_dataframe_generator.py
+++ CODE/server/sd_dataframe_generator.py
@@ -1,16 +1,29 @@
 import json
 import pandas as pd
+
+# Define the file(s) to load
 files = ["./merged_data.json"]
 
+# Create an empty list to store the symptom-disease pairs
 l = []
+
+# Loop through each file and extract the symptom-disease pairs
 for file in files:
     with open(file) as f:
+        # Load the JSON data from the file
         d = json.load(f)
+        # Loop through each object in the JSON data
         for obj in d:
+            # Check if the object contains both symptoms and diseases
             if "symptoms" in obj.keys() and "diseases" in obj.keys():
+                # Append a tuple of symptoms and diseases to the list
                 l.append({"Symptoms": tuple(obj["symptoms"]),"Diseases": tuple(obj["diseases"])})
-        
+
+# Convert the list of symptom-disease pairs to a pandas DataFrame
 df = pd.DataFrame.from_records(l)
+
+# Print information about the DataFrame
 print(df.info())
-    
+
+# Save the DataFrame as a pickle file
 df.to_pickle("./sdframe.pkl")
